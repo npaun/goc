509224703984d620bb59d031d02b95f56d2b11ec
msukkari <michael.sukkarieh@mail.mcgill.ca>
2019-03-01 16:19:43 -0500

    added tests for simple_stmt

--- programs/1-scan+parse/valid/simple_stmt.go
+++ programs/1-scan+parse/valid/simple_stmt.go
@@ -3,4 +3,27 @@ package main
 func main() {
 	x,y,z = 1,2,3
 	f,z := "wow", "another"
+
+	x += 5
+	x -= 5
+	x &= 5
+	x |= 5
+	x *= 5
+	x /= 5
+	x ^= 5
+	x <<= 5
+	x >>= 5
+	x %= 5
+	x &^= 5
+
+	x++
+	x--
+
+	print(x,y, y+10)
+	println(somefunc(), wow.x)
+
+	// expr are considered simple_stmt's
+	5 + 5
+	"wow"
+	-100
 }
--- src/pretty.ml
+++ src/pretty.ml
@@ -72,7 +72,7 @@ and string_of_stmt d tb stmt = match stmt.v with
 		| Expr(expr)                        -> (crt_tab d tb) ^ string_of_expr expr
 		| Block(blck)                       -> string_of_block (d+1) blck
 		| Assign(id_lst, expr_lst)          -> (crt_tab d tb) ^ (string_of_lst id_lst ", " (fun x -> x)) ^ " = " ^ (string_of_lst expr_lst ", " string_of_expr)
-		| OpAssign(id, op, expr)            -> (crt_tab d tb) ^ id ^ (string_of_op_assign op) ^ (string_of_expr expr)
+		| OpAssign(id, op, expr)            -> (crt_tab d tb) ^ id ^ " " ^ (string_of_op_assign op) ^ " " ^ (string_of_expr expr)
 		| IncDec(id, op)                    -> (crt_tab d tb) ^ id ^ (match op with `INC -> "++" | `DEC -> "--")
 		| Print(b, expr_lst)                -> (crt_tab d tb) ^ (if b then "println(" else "print(") ^ (string_of_lst expr_lst ", " string_of_expr) ^ ")"
 		| Return(expr_opt)                  -> (crt_tab d tb) ^ "return " ^ string_of_expr_opt expr_opt
