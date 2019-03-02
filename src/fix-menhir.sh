84f64aa7afac64695f3c85480728aa7329a08edc
Nicholas Paun <nicholaspaun@gmail.com>
2019-03-02 14:55:19 -0500

    Finish the menhir fixer

--- src/fix-menhir.sh
+++ src/fix-menhir.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
-if [ -d "menhir" ]; then 
+MENHIR_DIR="$(dirname $0)/menhir"
+MENHIR_VERSION="20181113"
+
+if [ -d $MENHIR_DIR ]; then 
 	exit 0
 fi
 
-mkdir menhir
-cd menhir
-wget https://gitlab.inria.fr/fpottier/menhir/-/archive/20181113/menhir-20181113.tar
-tar -xf menhir-20181113.tar
-cd menhir-20181113
-./configure
-make
+echo "Please wait we are downloading a new copy of menhir to make sure there are no problems"
+echo "This takes a minute or two"
 
+#wget https://gitlab.inria.fr/fpottier/menhir/-/archive/$MENHIR_VERSION/menhir-$MENHIR_VERSION.tar
+tar -xf menhir-$MENHIR_VERSION.tar
+mv menhir-$MENHIR_VERSION/ $MENHIR_DIR
+cd $MENHIR_DIR
+make PREFIX=.
+make PREFIX=. install
 
+echo "All done!"
