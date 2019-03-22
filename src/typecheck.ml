c1bfe1e517f6d6a33fb43b0c30e095d20c658974
L3gume <justin.tremblay@mail.mcgill.ca>
2019-03-22 11:28:35 -0400

    Implemented assignment statements, fixed an issue with if init-statements, made indentations more consistent

--- src/typecheck.ml
+++ src/typecheck.ml
@@ -1,152 +1,178 @@
 open Golite
 open Printf
 open Typelib
-
  
 let v_name = Pretty.string_of_lvalue'
 let maybe fn = function
-| Some arg -> Some (fn arg)
-| None -> None
+    | Some arg -> Some (fn arg)
+    | None -> None
 let present = function
-| Some _ -> true
-| None -> false
+    | Some _ -> true
+    | None -> false
 
 let default fn if_none = function
-| Some arg -> (fn arg)
-| None -> if_none
+    | Some arg -> (fn arg)
+    | None -> if_none
 
 let packify fn symt nodes =
 	List.map (fun n -> {v = n; _derived = []; _start = (-100,-100); _end = (-100,-100); _debug = "Packified"}) nodes
 	|> traverse fn symt
 	|> List.map (fun n -> n.v)
 
-
-
 let rec pass_ast symt = function
-| Program(pkg,tops) -> Program(pkg, traverse pass_toplevel (List.hd (descend symt)) tops)
+    | Program(pkg,tops) -> Program(pkg, traverse pass_toplevel (List.hd (descend symt)) tops)
 and pass_toplevel this_symt node  = function
-| Global(decl) -> same (fun () -> {node with v = Global(decl |> fwd_annot node |> pass_decl this_symt)})
-| Func(name,args,ret,body) -> down (fun child_symt -> {node with v = Func(name,args,ret, pass_block child_symt body)})
+    | Global(decl) -> same (fun () -> {node with v = Global(decl |> fwd_annot node |> pass_decl this_symt)})
+    | Func(name,args,ret,body) -> down (fun child_symt -> {node with v = Func(name,args,ret, pass_block child_symt body)})
 and pass_block this_symt body = traverse pass_statement this_symt body
 and pass_inner_stmt symt node = 
 	(* Sorry for the weird-ass function *)
 	(pass_block symt [node] |> List.hd)
-
 and pass_statement this_symt node = function
-| Decl(decls) -> same (fun () -> {node with v = Decl(List.map (fun d -> d |> fwd_annot node |> pass_decl this_symt) decls)})
-| Expr(expr) -> same (fun () -> {node with v = Expr(pass_expr this_symt expr)})
-| Return(expr) -> 
-	(* match with func return later *)
-	same (fun () -> {node with v = Return(maybe (pass_expr this_symt) expr)})
-| Block(stmts) -> down (fun child_symt -> {node with v = Block(pass_block child_symt stmts)})
-| For(pre,cond,post,block) ->
- 	down (fun child_symt -> 
-			{node with v = For(
-				(maybe (pass_inner_stmt child_symt) pre),
-				(maybe (pass_cond child_symt "for loop") cond),
-				(maybe (pass_inner_stmt child_symt) post),
-				pass_block (List.hd (descend child_symt)) block (* probably buggy af FIXME *)
-			)})
-| If(cases) -> down (fun child_symt -> {node with v = If(packify (pass_case "if condition" [`BOOL]) child_symt cases)})
-| Switch(stmt,cond,cases) -> down (fun child_symt ->
-		let cond' = (maybe (pass_expr child_symt) cond) in
-			let cond_t = (default typeof [`BOOL] cond') in
-				{node with v = Switch(
-					(maybe (pass_inner_stmt child_symt) stmt),
-					cond',
-					packify (pass_fallable_case "switch case" cond_t) child_symt cases
-				)}
-		)
-| Print(ln, exps) -> same (fun () ->
-	let exps' = List.map (pass_expr this_symt) exps in
-		List.iter (fun exp' -> assert_resolves_to_base this_symt "print statement" (typeof exp') exp') exps';
-		{node with v = Print(ln, exps')}
-	)
-| IncDec(arg, op) -> same (fun () ->
-	let arg' = pass_expr this_symt arg in
-		assert_is_numeric this_symt "increment or decrement statement" (typeof arg') arg';
-		{node with v = IncDec(arg', op)}
-	)
-| _ -> same (fun () -> node)
+    | Decl(decls) -> same (fun () -> {node with v = Decl(List.map (fun d -> d |> fwd_annot node |> pass_decl this_symt) decls)})
+    | Assign(assnlst) -> same (fun () -> {node with v = Assign(List.map (fun assn -> pass_assn this_symt assn) assnlst)})
+    | Expr(expr) -> same (fun () -> {node with v = Expr(pass_expr this_symt expr)})
+    | Return(expr) -> 
+        (* match with func return later *)
+        same (fun () -> {node with v = Return(maybe (pass_expr this_symt) expr)})
+    | Block(stmts) -> down (fun child_symt -> {node with v = Block(pass_block child_symt stmts)})
+    | For(pre,cond,post,block) ->
+        down (fun child_symt -> 
+                {node with v = For(
+                    (maybe (pass_inner_stmt child_symt) pre),
+                    (maybe (pass_cond child_symt "for loop") cond),
+                    (maybe (pass_inner_stmt child_symt) post),
+                    pass_block (List.hd (descend child_symt)) block (* probably buggy af FIXME *)
+                )})
+    | If(cases) -> down (fun child_symt -> {node with v = If(packify (pass_case "if condition" [`BOOL]) child_symt cases)})
+    | Switch(stmt,cond,cases) -> down (fun child_symt ->
+            let cond' = (maybe (pass_expr child_symt) cond) in
+                let cond_t = (default typeof [`BOOL] cond') in
+                    {node with v = Switch(
+                        (maybe (pass_inner_stmt child_symt) stmt),
+                        cond',
+                        packify (pass_fallable_case "switch case" cond_t) child_symt cases
+                    )}
+            )
+    | Print(ln, exps) -> same (fun () ->
+        let exps' = List.map (pass_expr this_symt) exps in
+            List.iter (fun exp' -> assert_resolves_to_base this_symt "print statement" (typeof exp') exp') exps';
+            {node with v = Print(ln, exps')}
+        )
+    | IncDec(arg, op) -> same (fun () ->
+        let arg' = pass_expr this_symt arg in
+            assert_is_numeric this_symt "increment or decrement statement" (typeof arg') arg';
+            {node with v = IncDec(arg', op)}
+        )
+    | _ -> same (fun () -> node)
 and pass_decl symt node = match node.v with
-| Var(name, lt, Some expr, s) ->
-	let expr' = pass_expr symt expr in
-		assert_match resolve_basic symt "variable declaration" (v_name name, [lt]) (expr, typeof expr');
-		Typerules.assert_redef_match symt node name (typeof expr');
-		Var(name, (type_single expr'), Some expr', s)
-| other -> other (* Type declarations are already analyzed by symtbl *)
+    | Var(name, lt, Some expr, s) ->
+        let expr' = pass_expr symt expr in
+        let lt' = infer_auto [lt] (typeof expr') in
+        Printf.printf "inferred var to type %s\n" (Pretty.string_of_typ (List.hd lt'));
+        assert_match resolve_basic symt "variable declaration" (v_name name, lt') (expr, typeof expr');
+        Typerules.assert_redef_match symt node name (typeof expr');
+        Var(name, (type_single expr'), Some expr', s)
+    | other -> other (* Type declarations are already analyzed by symtbl *)
+and pass_assn symt (lval, expr) = match (lval.v, expr.v) with
+    | (`Blank, oper)
+    | (`Indexing(_,_), oper)
+    | (`Selector(_,_), oper)
+    | (`V _, oper) -> (
+        let lval' = pass_lval symt lval in
+        let expr' = pass_expr symt expr in
+        assert_match resolve_basic symt "assignment" ("<lvalue>", typeof lval') (expr', typeof expr');
+        ({lval with v = lval'.v; _derived = typeof lval'}, {expr with v = oper; _derived = typeof expr'})
+    )
+    | _ -> failwith "non-lvalue expression on lhs of assignment"
 and pass_fallable_case ctx expected_t this_symt node = function
-| (case,mode) -> same (fun () -> {node with v = ((packify (pass_case ctx expected_t) this_symt [case] |> List.hd), mode)})
+    | (case, mode) -> same (fun () -> {node with v = ((packify (pass_case ctx expected_t) this_symt [case] |> List.hd), mode)})
 and pass_case ctx expected_t this_symt node = function
-| Default(block) -> down (fun child_symt -> {node with v = Default(pass_block child_symt block)})
-| Case(pre,conds,block) -> down (fun child_symt ->
-	let conds' = List.map (pass_expr this_symt) conds in
-		List.iter (fun cond' -> 
-			assert_match rt this_symt ctx ("<condition>",expected_t) (cond', typeof cond')
-		) conds';
-		{node with v = Case(
-				(maybe (pass_inner_stmt this_symt) pre),
-				conds',
-				(pass_block child_symt block)
-			)
-		}
-	)
+    | Default(block) -> down (fun child_symt -> {node with v = Default(pass_block child_symt block)})
+    | Case(pre,conds,block) -> 
+        let pre' = (maybe (pass_inner_stmt this_symt) pre) in
+        down (fun child_symt ->
+        let conds' = List.map (pass_expr this_symt) conds in
+            List.iter (fun cond' -> 
+                assert_match rt this_symt ctx ("<condition>",expected_t) (cond', typeof cond')
+            ) conds';
+            {node with v = Case(
+                    pre',
+                    conds',
+                    (pass_block child_symt block)
+                )
+            }
+        )
 and pass_cond symt ctx cond = 
 	let cond' = pass_expr symt cond in
 		assert_match rt symt ctx ("<condition>",[`BOOL]) (cond', typeof cond');
 		cond'
+and pass_lval symt node = match node.v with
+    | `Blank -> {node with v = `Blank; _derived = [`AUTO]}
+    | `V var -> {node with _derived = typeof_symbol symt var} 
+    | `Indexing(arr,idx) -> 
+        let arr' = pass_expr symt arr in
+        let idx' = pass_expr symt idx in
+            let elm_t = Typerules.element_type symt arr' in
+                assert_match rt symt "indexing" ("<index>", [`INT]) (idx', typeof idx');
+                {node with v = `Indexing(arr',idx'); _derived = elm_t}
+    | `Selector(obj,field) ->
+        let obj' = pass_expr symt obj in
+            let field_t = Typerules.field_type symt obj' field in
+                {node with v = `Selector(obj',field); _derived = field_t}
+    | _ -> failwith "non-value used on lhs of assignment, weeder failed"
 and pass_expr symt node = match node.v with
-| `L lit -> {node with _derived = [typeof_literal lit]}
-| `V var -> 
-	{node with _derived = typeof_symbol symt var} 
-| `Indexing(arr,idx) -> 
-	let arr' = pass_expr symt arr in
-	let idx' = pass_expr symt idx in
-		let elm_t = Typerules.element_type symt arr' in
-			assert_match rt symt "indexing" ("<index>", [`INT]) (idx', typeof idx');
-			{node with v = `Indexing(arr',idx'); _derived = elm_t}
-| `Selector(obj,field) ->
-	let obj' = pass_expr symt obj in
-		let field_t = Typerules.field_type symt obj' field in
-			{node with v = `Selector(obj',field); _derived = field_t}
-| `Call(_,_) -> pass_call symt node
-| `Op1(_,_) -> pass_op1 symt node
-| `Op2(_,_,_) -> pass_op2 symt node
+    | `L lit -> {node with _derived = [typeof_literal lit]}
+    | `V var -> 
+        {node with _derived = typeof_symbol symt var} 
+    | `Indexing(arr,idx) -> 
+        let arr' = pass_expr symt arr in
+        let idx' = pass_expr symt idx in
+            let elm_t = Typerules.element_type symt arr' in
+                assert_match rt symt "indexing" ("<index>", [`INT]) (idx', typeof idx');
+                {node with v = `Indexing(arr',idx'); _derived = elm_t}
+    | `Selector(obj,field) ->
+        let obj' = pass_expr symt obj in
+            let field_t = Typerules.field_type symt obj' field in
+                {node with v = `Selector(obj',field); _derived = field_t}
+    | `Call(_,_) -> pass_call symt node
+    | `Op1(_,_) -> pass_op1 symt node
+    | `Op2(_,_,_) -> pass_op2 symt node
 and pass_call symt node = match node.v with
-| `Call({v = `V "append"} as fn,[arr;elm]) -> 
-    (* TODO: hard to tell this actually works without finishing pass_expr, test later *)
-    let arr' = pass_expr symt arr in
-    let elm' = pass_expr symt elm in
-    let elmtyp = Typerules.slice_type symt arr' in
-    assert_match resolve_basic symt "call to append" ("<slice>", elmtyp) (elm', typeof elm');
-    {node with v = `Call(fn, [arr';elm']); _derived = typeof arr'}
-| `Call({v = `V "len"} as fn,[obj]) -> 
-	let obj' = pass_expr symt obj in
-		begin try 
-			let _ = Typerules.element_type symt obj' in ()
-		with 
-		| TypeError _ ->
-			assert_match rt symt "call to len" ("<argument>",[`STRING]) (obj', typeof obj');
-		end;
-		{node with v = `Call(fn, [obj']); _derived = [`INT]}
-| `Call({v = `V "cap"} as fn,[obj]) -> 
-	let obj' = pass_expr symt obj in
-		let _ = Typerules.element_type symt obj' in ();
-		{node with v = `Call(fn, [obj']); _derived = [`INT]}
-| `Call({v = `V "init"},_) -> raise (TypeError "The function init must not be called")
-| `Call({v = `V ident} as fn, args) ->
-    (* check if symbol kind is Symtbl.TypeK *)
-    (* if so, pass_cast instead *)    
-    if is_type_kind symt ident then
-        pass_cast symt node
-    else
-        let fn' = pass_expr symt fn in
-        let args' = List.map (pass_expr symt) args in
-        let typeof_args' = List.map typeof args' |> List.flatten in
-            let return_t = Typerules.return_type symt fn' in
-                assert_consist resolve_basic symt fn' (node, (return_t::typeof_args'));
-                {node with v = `Call(fn', args'); _derived = [return_t]}
-| _ -> raise (TypeError (sprintf "Expression '%s' cannot yield a callable value %s" (Pretty.string_of_expr node) (err_loc node)))
+    | `Call({v = `V "append"} as fn,[arr;elm]) -> 
+        (* TODO: hard to tell this actually works without finishing pass_expr, test later *)
+        let arr' = pass_expr symt arr in
+        let elm' = pass_expr symt elm in
+        let elmtyp = Typerules.slice_type symt arr' in
+        assert_match resolve_basic symt "call to append" ("<slice>", elmtyp) (elm', typeof elm');
+        {node with v = `Call(fn, [arr';elm']); _derived = typeof arr'}
+    | `Call({v = `V "len"} as fn,[obj]) -> 
+        let obj' = pass_expr symt obj in
+            begin try 
+                let _ = Typerules.element_type symt obj' in ()
+            with 
+            | TypeError _ ->
+                assert_match rt symt "call to len" ("<argument>",[`STRING]) (obj', typeof obj');
+            end;
+            {node with v = `Call(fn, [obj']); _derived = [`INT]}
+    | `Call({v = `V "cap"} as fn,[obj]) -> 
+        let obj' = pass_expr symt obj in
+            let _ = Typerules.element_type symt obj' in ();
+            {node with v = `Call(fn, [obj']); _derived = [`INT]}
+    | `Call({v = `V "init"},_) -> raise (TypeError "The function init must not be called")
+    | `Call({v = `V ident} as fn, args) ->
+        (* check if symbol kind is Symtbl.TypeK *)
+        (* if so, pass_cast instead *)    
+        if is_type_kind symt ident then
+            pass_cast symt node
+        else
+            let fn' = pass_expr symt fn in
+            let args' = List.map (pass_expr symt) args in
+            let typeof_args' = List.map typeof args' |> List.flatten in
+                let return_t = Typerules.return_type symt fn' in
+                    assert_consist resolve_basic symt fn' (node, (return_t::typeof_args'));
+                    {node with v = `Call(fn', args'); _derived = [return_t]}
+    | _ -> raise (TypeError (sprintf "Expression '%s' cannot yield a callable value %s" (Pretty.string_of_expr node) (err_loc node)))
 and pass_cast symt node = 
     let bad_cast msg = let (line, col) = node._start in
         raise (SyntaxError ("in cast expression at line " ^ string_of_int line ^ ", char " ^ string_of_int col ^ ", " ^ msg))
@@ -176,7 +202,8 @@ and pass_cast symt node =
         {node with v = `Cast(List.hd dest_typ, obj'); _derived = dest_typ}
     | `Call(_, lst) -> (match lst with 
         | [] -> bad_cast "expected expression"
-        | h::h'::t -> bad_cast "only one expression can be cast")
+        | h::h'::t -> bad_cast "only one expression can be cast"
+    )
 and pass_op1 symt node =
 	let op1_assertion = function
 	| `POS -> (fun n -> assert_is_numeric symt "unary +" (typeof n) n)
@@ -204,4 +231,3 @@ and pass_op2 symt node =
 			{node with v = `Op2(op,a',b'); _derived = op_t}
 	| `Op2(`ADD,a,b) -> {node with _derived = [`VOID]} (* string, etc. *)
 	| rest -> {node with _derived = [`VOID]} (* numeric, comparable, ordered operations *)
-
