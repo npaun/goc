cac5d8b17ac7e3d9f66f5f7628a4c56f46e2761d
L3gume <justin.tremblay@mail.mcgill.ca>
2019-03-27 21:47:36 -0400

    Added the 3 files for milestone 3

---
+++ programs/3-semantics+codegen/valid/short-var-decl.go
@@ -0,0 +1,18 @@
+//~5
+//~6
+//~7
+//~8
+//~9
+
+func main() {
+    a, b, c := 5, 6, 7
+
+    println(a)
+    println(b)
+    println(c)
+
+    // redeclaration of vars in the same scope
+    a, c := 8, 9
+    println(a)
+    println(c)
+}
